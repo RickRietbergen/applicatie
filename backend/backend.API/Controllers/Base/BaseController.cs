8d47218fb18288deaf82fa1fbedb1ff104cdae1b
Rick Rietbergen <rbps3.2016@gmail.com>
2024-11-01 00:32:54 +0100

    XUnit

---
+++ backend/backend.API/Controllers/Base/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.API.Controllers.Base
+{
+    public class BaseController : ControllerBase
+    {
+        protected bool TryGetUserId(out int? userId)
+        {
+            var result = HttpContext.Items.TryGetValue("user", out var user);
+
+            if (result)
+            {
+                userId = (int)user;
+                return true;
+            }
+            else
+            {
+                userId = null;
+                return false;
+            }
+        }
+
+        /*protected bool TryGetUserRoles(out Role? userRoles)
+        {
+            var result = HttpContext.Items.TryGetValue("roles", out var roles);
+
+            if (result)
+            {
+                userRoles = (Role)roles;
+                return true;
+            }
+            else
+            {
+                userRoles = null;
+                return false;
+            }
+        }*/
+    }
+}
